9e7633478fd2b19054cef685678fd2a119d144f2
Arnau <arnaugarciaolmos@gmail.com>
2018-05-29 16:41:55 +0200

    Base de dades final

--- app/src/main/java/upf/edu/smartpills/MainActivity.java
+++ app/src/main/java/upf/edu/smartpills/MainActivity.java
@@ -3,6 +3,10 @@ package upf.edu.smartpills;
 import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
