24ba3949177276971eb48f6e2aa2e4d1f7f1071f
jorge <jorgesalv.cordova@gmail.com>
2018-05-30 00:16:33 +0200

    Esto es la prueba

--- app/src/main/java/upf/edu/smartpills/CalendarActivity.java
+++ app/src/main/java/upf/edu/smartpills/CalendarActivity.java
@@ -1,21 +1,27 @@
 package upf.edu.smartpills;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class CalendarActivity extends AppCompatActivity {
 
 
     private DrawerLayout myDrawer;
     private ActionBarDrawerToggle myToggle;
+    private TextView nombre;
+    FloatingActionButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-
+        //Menu Icon
         myDrawer = findViewById(R.id.myDrawer);
         myToggle = new ActionBarDrawerToggle(this,myDrawer,R.string.open,R.string.close);
 
@@ -23,6 +29,21 @@ public class CalendarActivity extends AppCompatActivity {
         myToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Getting the name
+        //nombre = findViewById(R.id.textView);
+        //nombre.append(" "+USER.GETNAME);
+
+        button = findViewById(R.id.floatingActionButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profile = new Intent(CalendarActivity.this, TreatmentsActivity.class);
+                startActivity(profile);
+                finish();
+            }
+        });
+
     }
 
     @Override
@@ -31,5 +52,11 @@ public class CalendarActivity extends AppCompatActivity {
            return true;
 
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        //NavUtils.navigateUpFromSameTask(this);
+        //finish();
     }
 }
