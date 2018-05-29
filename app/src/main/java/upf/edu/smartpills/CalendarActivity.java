e2dc073ba852efcc088a2900be796030bd57959a
Miquel <miquel.puerto01@estudiant.upf.edu>
2018-05-29 16:54:38 +0200

    Incorporació del projecte d'en Jorge.

---
+++ app/src/main/java/upf/edu/smartpills/CalendarActivity.java
@@ -0,0 +1,13 @@
+package upf.edu.smartpills;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
