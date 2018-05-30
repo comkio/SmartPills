2a2af540812daf1a547d0055e4bae780d3dd70b6
jorge <jorgesalv.cordova@gmail.com>
2018-05-30 12:30:33 +0200

    Esto es la prueba

--- app/src/main/java/upf/edu/smartpills/NewTreatmentActivity.java
+++ app/src/main/java/upf/edu/smartpills/NewTreatmentActivity.java
@@ -3,6 +3,7 @@ package upf.edu.smartpills;
 import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -19,6 +20,7 @@ public class NewTreatmentActivity extends AppCompatActivity {
         final ViewGroup transitionsContainer = findViewById(R.id.transitions_container);
         final TextView text = transitionsContainer.findViewById(R.id.text);
         final Button button = transitionsContainer.findViewById(R.id.button);
+        final LinearLayoutCompat linearLayoutCompat = findViewById(R.id.layout2);
         button.setOnClickListener(new View.OnClickListener() {
 
             boolean visible;
@@ -27,7 +29,8 @@ public class NewTreatmentActivity extends AppCompatActivity {
             public void onClick(View v) {
                 TransitionManager.beginDelayedTransition(transitionsContainer);
                 visible = !visible;
-                text.setVisibility(visible ? View.VISIBLE : View.GONE);
+                linearLayoutCompat.setVisibility(visible ? View.VISIBLE : View.GONE);
+
             }
 
         });
--- app/src/main/java/upf/edu/smartpills/TreatmentsActivity.java
+++ app/src/main/java/upf/edu/smartpills/TreatmentsActivity.java
@@ -5,10 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TreatmentsActivity extends AppCompatActivity {
 
 
+
+    ListView treatmentList;
+    ListView pillsList;
     FloatingActionButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +23,7 @@ public class TreatmentsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_treatments);
 
 
-
+        //Button Action to another activity
         button = findViewById(R.id.floatingActionButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +34,27 @@ public class TreatmentsActivity extends AppCompatActivity {
             }
         });
 
+        treatmentList = findViewById(R.id.treatmentsList);
+        pillsList = findViewById(R.id.pillsList);
+
+        //Putting the treatments name into the listview***
+        //ArrayList<String> treatments = new ArrayList<>();
+        //get all treatment names of db
+        //String[] treatmentsName = db.getTreatmentsName()?;
+        //for (int i = 0; i < treamentsName.length; ++i) {
+        //    treatments.add(values[i]);
+        //}
+        //ListAdapter adapter = new ListAdapter(this, android.R.layout.simple_list_item_1, treatments);
+        //treatmentList.setAdapter(adapter);
+
+        //Putting the pills name into the listview***
+        //ArrayList<String> pills = new ArrayList<>();
+        //get all pill names of db
+        //String[] pillsName = db.getTreatmentsName()?;
+        //for (int i = 0; i < pillsName.length; ++i) {
+        //    pills.add(values[i]);
+        //}
+        //ListAdapter adapter = new ListAdapter(this, android.R.layout.simple_list_item_1, pills);
+        //pillList.setAdapter(adapter);
     }
 }
