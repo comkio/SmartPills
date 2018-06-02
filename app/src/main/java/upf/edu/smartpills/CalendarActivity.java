35075bd8d389b2963a0b59c5b66f60b38e2fd882
jorge <jorgesalv.cordova@gmail.com>
2018-06-02 22:05:03 +0200

    ListView done,just need the room functions. Parent working but return to the welcome activity dont know why

--- app/src/main/java/upf/edu/smartpills/CalendarActivity.java
+++ app/src/main/java/upf/edu/smartpills/CalendarActivity.java
@@ -2,6 +2,7 @@ package upf.edu.smartpills;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -13,7 +14,9 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -26,7 +29,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class CalendarActivity extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity implements About.OnFragmentInteractionListener{
 
 
     private DrawerLayout myDrawer;
@@ -47,12 +50,53 @@ public class CalendarActivity extends AppCompatActivity {
         //Menu Icon
         myDrawer = findViewById(R.id.myDrawer);
         myToggle = new ActionBarDrawerToggle(this,myDrawer,R.string.open,R.string.close);
-
+        NavigationView nv = findViewById(R.id.nv);
         myDrawer.addDrawerListener(myToggle);
         myToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        nv.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        boolean fragmentTransaction = false;
+                        Fragment fragment = null;
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.about:
+                                fragment = new About();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.umaual:
+                                fragment = new UserManual();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.settings:
+                                fragment = new Settings();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.restart:
+                                fragment = new Restart();
+                                fragmentTransaction = true;
+                                break;
+
+                        }
+                        if(fragmentTransaction) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.flcontent, fragment)
+                                    .commit();
+
+                            menuItem.setChecked(true);
+                            getSupportActionBar().setTitle(menuItem.getTitle());
+                        }
+
+                        myDrawer.closeDrawers();
+
+                        return true;
+                    }
+                });
 
         //Getting the name
         //nombre = findViewById(R.id.textView);
@@ -108,17 +152,20 @@ public class CalendarActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my_menu, menu);
+        //getMenuInflater().inflate(R.menu.my_menu, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
         return true;
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
+        switch(item.getItemId()) {
             case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
+                myDrawer.openDrawer(GravityCompat.START);
                 return true;
+            //...
         }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -178,5 +225,10 @@ public class CalendarActivity extends AppCompatActivity {
     @Override
     public Intent getParentActivityIntent(){
         return getSupportParentActivityIntent();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
