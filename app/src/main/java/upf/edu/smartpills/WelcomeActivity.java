5f25a0948a84bb58dfead89b5d99d2fbf6d9be38
Miquel <miquel.puerto01@estudiant.upf.edu>
2018-06-02 19:18:20 +0200

    He posat imatges per l'animació a res.drawable He creat loading.xml a res.drawable per fer l'animació He inserit a activity_welcome.xml un ImageView He modificat WelcomeActivity.java perque faci algo al clicar

--- app/src/main/java/upf/edu/smartpills/WelcomeActivity.java
+++ app/src/main/java/upf/edu/smartpills/WelcomeActivity.java
@@ -1,7 +1,14 @@
 package upf.edu.smartpills;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +16,27 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        //Inici variables per fer el gir.
+        final ViewGroup transitionsContainer = findViewById(R.id.transitions_container);
+        final Button button = transitionsContainer.findViewById(R.id.button_nom);
+        final EditText edit =  (EditText) findViewById(R.id.editTextW);
+
+        //Gestionar la introducció del nom
+
+
+        //Quan es pitja el boto, que giri i canvii de activity
+        button.setOnClickListener(new View.OnClickListener() {
+            ImageView loading = (ImageView) findViewById(R.id.imageView);
+            AnimationDrawable animation = (AnimationDrawable) loading.getDrawable();
+            String nom;
+
+            public void onClick(View v) {
+                nom = edit.getText().toString();
+                animation.start();
+                startActivity(new Intent(WelcomeActivity.this, CalendarActivity.class));
+            }
+
+        });
+
     }
 }
