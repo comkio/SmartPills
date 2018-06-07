f9e978ac1904c14e6f130e9eabfc886ecfb0446d
MartiGJ <martigj94@gmail.com>
2018-06-08 00:27:26 +0200

    Menu working!!!!!!!!

--- app/src/main/java/upf/edu/smartpills/CalendarActivity.java
+++ app/src/main/java/upf/edu/smartpills/CalendarActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,92 +31,33 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class CalendarActivity extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
 
-    private DrawerLayout myDrawer;
-    private ActionBarDrawerToggle myToggle;
-    private TextView nombre;
     FloatingActionButton button;
-    TextView  day;
-    Button btnCamera;
+    TextView day;
     ImageView imageCamera;
     ListView listCalendar;
+    private TextView nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         listCalendar = findViewById(R.id.listCalendar);
         //Menu Icon
-        myDrawer = findViewById(R.id.myDrawer);
-        myToggle = new ActionBarDrawerToggle(this,myDrawer,R.string.open,R.string.close);
-        NavigationView nv = findViewById(R.id.nv);
+        DrawerLayout myDrawer = findViewById(R.id.myDrawer);
+        ActionBarDrawerToggle myToggle = new ActionBarDrawerToggle(
+                this,myDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         myDrawer.addDrawerListener(myToggle);
         myToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-        nv.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                        FragmentManager fragmentManager = getSupportFragmentManager();
-                        boolean fragmentTransaction = false;
-                        Fragment fragment = null;
-
-                        switch (menuItem.getItemId()) {
-                            case R.id.about:
-                                fragmentManager.beginTransaction().replace(R.id.flcontent,new About()).commit();
-                                //fragment = new About();
-                                //fragmentTransaction = true;
-                                break;
-                            case R.id.umaual:
-                                fragmentManager.beginTransaction().replace(R.id.flcontent,new UserManual()).commit();
-                                //fragment = new UserManual();
-                                //fragmentTransaction = true;
-                                break;
-                            case R.id.settings:
-                                fragmentManager.beginTransaction().replace(R.id.flcontent,new Settings()).commit();
-                                //fragment = new Settings();
-                                //fragmentTransaction = true;
-                                break;
-                            case R.id.restart:
-                                fragmentManager.beginTransaction().replace(R.id.myDrawer,new Restart()).commit();
-                                //fragment = new Restart();
-                                //fragmentTransaction = true;
-                                break;
-                            case R.id.btnCamera:
-                                btnCamera = findViewById(R.id.btnCamera);
-                                imageCamera = findViewById(R.id.imageCamera);
-                                btnCamera.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                                        startActivityForResult(intent,0);
-                                    }
-                                });
-                                break;
-
-
-                        }
-                        if(fragmentTransaction) {
-                            getSupportFragmentManager().beginTransaction()
-                                    .replace(R.id.flcontent, fragment)
-                                    .commit();
-
-                            menuItem.setChecked(true);
-                            getSupportActionBar().setTitle(menuItem.getTitle());
-                        }
-
-                        myDrawer.closeDrawers();
-
-                        return true;
-                    }
-                });
+        NavigationView nv = findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(this);
 
         //Getting the name
         //nombre = findViewById(R.id.textView);
@@ -135,7 +77,7 @@ public class CalendarActivity extends AppCompatActivity {
 
         //CURRENT DATE
         String mydate = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
-        String[] mydat  = mydate.split(",");
+        String[] mydat = mydate.split(",");
         day = findViewById(R.id.textView2);
         day.append(mydat[0]);
 
@@ -146,69 +88,53 @@ public class CalendarActivity extends AppCompatActivity {
         //final ArrayList<String> list = new ArrayList<String>();
         //for (int i = 0; i < todayPills.length; ++i) {
 //            list.add(todayPills[i]);
-  //      }
-    //    ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
-      //  listCalendar.setAdapter(adapter);
-
+        //      }
+        //    ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        //  listCalendar.setAdapter(adapter);
 
 
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.my_menu, menu);
-      //  MenuInflater inflater = getMenuInflater();
-        //inflater.inflate(R.menu.my_menu, menu);
-        return true;
-    }
+    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (myToggle.onOptionsItemSelected(item)) {
-            return true;
-        }else {
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
 
-            switch(item.getItemId()) {
-                case R.id.about:
-                    Log.d("ActionItemClicked", "About clicked********");
-                    return true;
-                case R.id.umaual:
-                    Log.d("ActionItemClicked", "User Manual clicked");
-                    return true;
-                case R.id.settings:
-                    Log.d("ActionItemClicked", "Settings clicked");
-                    return true;
-                case R.id.restart:
-                    Log.d("ActionItemClicked", "Restart clicked");
-                    return true;
-            }
+        int id = menuItem.getItemId();
+
+        if (id == R.id.about) {
+
+        } else if (id == R.id.umanual) {
+            CharSequence text = "Works!";
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.settings) {
+
+        } else if (id == R.id.restart) {
+
         }
 
+        DrawerLayout myDrawer = (DrawerLayout) findViewById(R.id.myDrawer);
+        myDrawer.closeDrawer(GravityCompat.START);
 
-        return super.onOptionsItemSelected(item);
+        return true;
     }
-
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.myDrawer);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
+
     //Picture Function
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
         imageCamera.setImageBitmap(bitmap);
     }
 
-    @Override
-    public Intent getSupportParentActivityIntent(){
-        return getSupportParentActivityIntent();
-    }
-    @Override
-    public Intent getParentActivityIntent(){
-        return getSupportParentActivityIntent();
-    }
 
 }
